b8445023df752f8997cb645278ec48544a0e00cd
SaikoRobot <masaki.natsuki@saikorobot.com>
2013-03-15 14:52:43 +0900

    とりあえず版
    layoutからfragmentをFrameLayoutへ変更
    コードからfragmentを挿入する
    
    layoutを修正

--- src/jp/eure/android/sample/FragmentPattern/activity/MainActivity.java
+++ src/jp/eure/android/sample/FragmentPattern/activity/MainActivity.java
@@ -5,34 +5,39 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import jp.eure.android.sample.FragmentPattern.R;
 import jp.eure.android.sample.FragmentPattern.fragment.MainFragment;
+import jp.eure.android.sample.FragmentPattern.util.DLog;
 
 /**
- * Created with IntelliJ IDEA.
- * User: pixie3
- * Date: 2013/03/14
- * Time: 12:06
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: pixie3 Date: 2013/03/14 Time: 12:06 To
+ * change this template use File | Settings | File Templates.
  */
-public class MainActivity extends BaseActivity implements MainFragment.OnCurrentPageChangeListener {
+public class MainActivity extends BaseActivity implements
+	MainFragment.OnCurrentPageChangeListener {
 
-	private MainFragment mMainFragment;
-	private FragmentManager mFragmentManger;
-	private FragmentTransaction mFragmentTrasaction;
+    private MainFragment mMainFragment;
+    private FragmentManager mFragmentManger;
+    private FragmentTransaction mFragmentTrasaction;
 
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_view);
-	}
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main_view);
+	DLog.d("MainActivity onCreate");
+	mFragmentManger = getSupportFragmentManager();
+	mFragmentTrasaction = mFragmentManger.beginTransaction();
+	mMainFragment = MainFragment.newInstance();
+	mFragmentTrasaction.replace(R.id.main_fragment, mMainFragment);
+	mFragmentTrasaction.addToBackStack(null);
+	mFragmentTrasaction.commit();
+    }
 
-	@Override
-	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-		mMainFragment = (MainFragment)getSupportFragmentManager().findFragmentById(R.id.main_fragment);
-		mMainFragment.setPage(itemPosition);
-		return super.onNavigationItemSelected(itemPosition, itemId);
-	}
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+	mMainFragment.setPage(itemPosition);
+	return super.onNavigationItemSelected(itemPosition, itemId);
+    }
 
-	@Override
-	public void onChangeCurrentPage(int currentPage) {
-		mActionBar.setSelectedNavigationItem(currentPage);
-	}
+    @Override
+    public void onChangeCurrentPage(int currentPage) {
+	mActionBar.setSelectedNavigationItem(currentPage);
+    }
 }
